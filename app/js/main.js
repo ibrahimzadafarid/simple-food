a77522de1ef1faef5f5fdf411dd9b9eeef879495
Farid Ibrahimzada <ibrahimzada.farid@gmail.com>
2024-11-23 12:15:06 +0400

    finish header, category, footer, add mixitup

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,14 @@
-$(function () {
+$(function () {});
 
+document.addEventListener('DOMContentLoaded', () => {
+  const filterBtn = document.querySelectorAll('.popular__filter-btn');
 
-})
+  filterBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      filterBtn.forEach((button) => button.classList.remove('popular__filter-btn--active'));
+      btn.classList.add('popular__filter-btn--active');
+    });
+  });
+});
+
+var mixer = mixitup('.popular__content');
